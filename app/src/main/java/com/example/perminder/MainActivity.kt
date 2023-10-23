9927f68cc8b962a692f5ab455644da04d7d8f1ee
lolosityftw <lolosityftw5@gmail.com>
2023-10-24 00:29:39 +0100

    Added pages and basic task card

--- app/src/main/java/com/example/perminder/MainActivity.kt
+++ app/src/main/java/com/example/perminder/MainActivity.kt
@@ -3,31 +3,38 @@ package com.example.perminder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.perminder.ui.theme.PerminderTheme
+import android.content.res.Configuration
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello World")
+           TaskCard(Task("Android", "Jetpack Compose"))
             }
         }
     }
 
+data class Task(val title: String, val body: String)
+
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hellow $name")
+fun TaskCard(task: Task) {
+    Column {
+        Text(text = task.title)
+        Text(text = task.body)
+    }
 }
+
 
 @Preview
 @Composable
-fun PreviewMessageCard() {
-    MessageCard("Android")
+fun PreviewTaskCard() {
+    TaskCard(
+        task = Task("Clean up", "Clean your room")
+    )
 }
